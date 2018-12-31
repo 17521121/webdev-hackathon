1397c6901a6c0388bb25ef77475244244fdb7cda
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2018-12-31 15:56:01 +0700

    Statistic

--- routes/adminpage/index.js
+++ routes/adminpage/index.js
@@ -3,8 +3,25 @@ var mongoose = require('mongoose');
 var { checkPermission } = require('../../services/checkPermission');
 var { IS_USER } = require('../../config/constants')
 
-router.get('/', checkPermission(IS_USER), async (req, res, next) => {
-  return res.render('adminpage')
+router.get('/', checkPermission(IS_USER),async (req, res, next) => {
+  let countUsers = await  mongoose.model("users").count();
+ 
+  //so luong teams đã đăng kí
+  let countRegister =  await mongoose.model("teams").count();
+  
+  //số lượng đã nộp vòng 1: 
+  let teams = await mongoose.model("teams").find();
+  let countJoined = 0;
+
+  for (const team of teams) {
+    for(const submission of team.submissions) {
+      if(submission.score > 0) {
+        countJoined += 1;
+        break;
+      }
+    }
+  }
+  return res.render('adminpage', { countUsers, countRegister,countJoined } );
 })
 
 require('./teams')(router);
