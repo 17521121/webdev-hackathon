508627fe1ce97afedf823af55fb4cb633b2ebc97
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2019-01-04 22:33:27 +0700

    Submit code theo đợt

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,5 @@
 module.exports = {
-  IS_USER: ["*"]
+  IS_USER: ["*"],
+  deadline_vong1: Date("2019-02-15 12:00:00"),
+  MAX_FILE_SIZE_FOR_UPLOAD: 150*1024*1024
 }
--- routes/homepage/submit/index.js
+++ routes/homepage/submit/index.js
@@ -1,10 +1,24 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 var multer = require('multer')
+var { constants } = require('../../../config/constants');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'vong1')
+  },
+  onFileUploadStart: function (file) {
+    // You now have access to req
+    var now = new Date();
+    if (now > constants.deadline_vong1)
+      return false;
+  },
+  onFileSizeLimit: function (file) {
+    res.send('> The file size exceeds the maximum size allowed (' + constants.MAX_FILE_SIZE_FOR_UPLOAD / (1024 * 1024) + 'MB)');
+  },
+  onFileUploadComplete: function (file) {
+    //console.log('> File \'' + file.fieldname + '\' stored in \'' + file.path + '\'' + (file.truncated ? ' (TRUNCATED).' : '.') );
+    res.send('> File' + file.fieldname + 'has been uploaded');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
@@ -20,7 +34,9 @@ module.exports = router => {
     return res.render('homepage/submit/', { sponsors });
   })
 
-  router.post('/submit/vong1', upload.single('file1'), (req, res) => {
+  router.post('/submit/vong1', upload.single('file1'), async (req, res) => {
+    // Tìm team và sửa đường dẫn
+    console.log(req.file.path)
     return res.redirect("/");
   });
 }
