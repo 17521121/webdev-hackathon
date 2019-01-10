eca733d0d8c11f1115446d27521d68631f6a0bc0
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2019-01-10 13:43:54 +0700

    deadline and limits size for submit

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   IS_USER: ["*"],
-  deadline_vong1: Date("2019-02-15 12:00:00"),
+  deadline_vong1:new Date("2019-02-15 12:00:00"),
   MAX_FILE_SIZE_FOR_UPLOAD: 150*1024*1024,
   category: ["Thông báo", "Css", "Html", "Javascripts", "NodeJs", "Php",".Net", "Java"],
   PLATFORMS: ["Node.js","Java","PHP",".NET"]
--- routes/homepage/submit/index.js
+++ routes/homepage/submit/index.js
@@ -1,43 +1,35 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 var multer = require('multer')
-var { constants } = require('../../../config/constants');
+var constants = require('../../../config/constants');
 var check= require('../../../services/checkTeamPermission');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'vong1')
   },
-  onFileUploadStart: function (file) {
-    // You now have access to req
-    var now = new Date();
-    if (now > constants.deadline_vong1)
-      return false;
-  },
-  onFileSizeLimit: function (file) {
-    res.send('> The file size exceeds the maximum size allowed (' + constants.MAX_FILE_SIZE_FOR_UPLOAD / (1024 * 1024) + 'MB)');
-  },
-  onFileUploadComplete: function (file) {
-    //console.log('> File \'' + file.fieldname + '\' stored in \'' + file.path + '\'' + (file.truncated ? ' (TRUNCATED).' : '.') );
-    res.send('> File' + file.fieldname + 'has been uploaded');
-  },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
   }
 })
-
-var upload = multer({ storage: storage })
+var upload = multer({ 
+  storage: storage,
+  limits: { fileSize: constants.MAX_FILE_SIZE_FOR_UPLOAD } 
+})
 
 module.exports = router => {
   //get dang ki
   router.get('/submit/vong1', check.checkTeamPermission, async (req, res, next) => {
     let sponsors = await mongoose.model('sponsors').find();
-    return res.render('homepage/submit', { sponsors });
+    return res.render('homepage/submit', { sponsors, deadline: constants.deadline_vong1 });
   })
 
-  router.post('/submit/vong1', check.checkTeamPermission, upload.single('file1'), async (req, res) => {
-    // Tìm team và sửa đường dẫn
-    console.log(req.file.path)
-    return res.redirect("/");
-  });
+  router.post('/submit/vong1', check.checkTeamPermission, 
+    (req, res, next) => {
+      return( now - deadline_vong1 < 0 )? next() : res.redirect('/submit/vong1') ;
+    }, upload.single('file1'), async (req, res) => {
+      // Tìm team và sửa đường dẫn
+      //console.log(req.file.path)
+      return res.redirect("/");
+    });
 }
