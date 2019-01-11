ce88755927b84ac8ab8b64996b7138035f140e62
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2019-01-11 18:49:28 +0700

    about contest

--- routes/homepage/index.js
+++ routes/homepage/index.js
@@ -9,6 +9,16 @@ router.get('/', async (req, res, next) => {
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage', { sponsors, news, teamLogin });
 })
+router.get('/info/lien-he', async (req, res, next) => {
+	let sponsors = await mongoose.model('sponsors').find();
+	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
+	return res.render('homepage/inform/contact', { sponsors, teamLogin });
+})
+router.get('/info/fag', async (req, res, next) => {
+	let sponsors = await mongoose.model('sponsors').find();
+	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
+	return res.render('homepage/inform/fag', { sponsors, teamLogin });
+})
 
 require('./register')(router)
 require('./login-out')(router)
