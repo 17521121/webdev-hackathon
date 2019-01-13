426c65b7b9923f29125ab75d6d467ba5d7bf9f4c
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2019-01-13 21:45:14 +0700

    edit

--- routes/homepage/index.js
+++ routes/homepage/index.js
@@ -9,35 +9,35 @@ router.get('/', async (req, res, next) => {
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage', { sponsors, news, teamLogin });
 })
-router.get('/info/lien-he', async (req, res, next) => {
+router.get('/lien-he', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/contact', { sponsors, teamLogin });
 })
-router.get('/info/fag', async (req, res, next) => {
+router.get('/faq', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/fag', { sponsors, teamLogin });
 })
-router.get('/info/fag/the-le', async (req, res, next) => {
+router.get('/the-le', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/rules', { sponsors, teamLogin });
 })
 //about
-router.get('/info/fag/ve-chung-toi', async (req, res, next) => {
+router.get('/ve-chung-toi', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/about', { sponsors, teamLogin });
 })
 //plan
-router.get('/info/fag/ke-hoach', async (req, res, next) => {
+router.get('/ke-hoach', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/plans', { sponsors, teamLogin });
 })
 //rule-mark
-router.get('/info/fag/cham-diem', async (req, res, next) => {
+router.get('/cham-diem', async (req, res, next) => {
 	let sponsors = await mongoose.model('sponsors').find();
 	let teamLogin = await mongoose.model('teams').findById(req.signedCookies.team);
 	return res.render('homepage/inform/rule-mark', { sponsors, teamLogin });
